160e44d97b7c521a19372a59e437d039c18b1426
marcorios26 <riosmar026@gmail.com>
2021-09-08 20:12:14 -0400

    adding some documents at my project

---
+++ main.js
@@ -0,0 +1,7 @@
+(function() {
+    let movil = document.getElementById('movil');
+    let menu = document.getElementById('principal');
+    movil.addEventListener('click', function() {
+        menu.style.display = menu.style.display == 'flex' ? 'none' : 'flex';
+    });
+})();
